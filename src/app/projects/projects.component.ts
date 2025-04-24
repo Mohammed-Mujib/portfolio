339cadff0f7145eaa21bb592a56c2b8c587d2afd
Mohammed-Mujib <wadmujib@gmail.com>
2025-04-24 17:34:15 +0200

    finish moblie design

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
 })
@@ -11,62 +12,70 @@ export class ProjectsComponent {
   projects: any[] = [
     {
       name: 'Manga-hala',
-      photo: '',
+      photo: 'assets/projects_screenshot/manga-halan.png',
       skills: ['html', 'css', 'tailwindcss', 'php', 'laravel'],
       link: '',
     },
     {
       name: 'BudgetTracker',
-      photo: '',
-      skills: ['html', 'css', 'bootstrap', 'Angular', 'python', 'django'],
+      photo: 'assets/projects_screenshot/budget-traker.png',
+      skills: [
+        'html',
+        'css',
+        'bootstrap',
+        'Typescript',
+        'Angular',
+        'python',
+        'django',
+      ],
       link: '',
     },
     {
       name: 'Recipes-book',
-      photo: '',
+      photo: 'assets/projects_screenshot/recipes-book.png',
       skills: ['html', 'css', 'bootstrap', 'javaScript'],
       link: 'https://mohammed-mujib.github.io/Recipes-book/',
     },
     {
+      name: 'Interior Design',
+      photo: 'assets/projects_screenshot/interior-design.png',
+      skills: ['html', 'css', 'bootstrap', 'Typescript', 'Angular'],
+      link: 'https://mohammed-mujib.github.io/Interior-Design/showcase',
+    },
+    {
       name: 'Weather-API',
-      photo: '',
+      photo: 'assets/projects_screenshot/weather-api.png',
       skills: ['html', 'css', 'bootstrap', 'javaScript'],
       link: 'https://mohammed-mujib.github.io/Weather-API/',
     },
     {
       name: 'envatoMarket',
-      photo: '',
+      photo: 'assets/projects_screenshot/evantoMarket.png',
       skills: ['html', 'css', 'bootstrap', 'javaScript'],
       link: 'https://mohammed-mujib.github.io/envatoMarket/',
     },
     {
       name: 'Travela',
-      photo: '',
+      photo: 'assets/projects_screenshot/travela.png',
       skills: ['html', 'css', 'bootstrap'],
       link: 'https://mohammed-mujib.github.io/Travela/',
     },
     {
-      name: 'Interior Design',
-      photo: '',
-      skills: ['html', 'css', 'bootstrap'],
-      link: 'https://mohammed-mujib.github.io//',
-    },
-    {
       name: 'Calculator',
-      photo: '',
+      photo: 'assets/projects_screenshot/calculator.png',
       skills: ['html', 'css', 'bootstrap', 'javaScript'],
       link: 'https://mohammed-mujib.github.io/Calculator/',
     },
     {
       name: 'Tic Tac Toe',
-      photo: '',
-      skills: ['html', 'css', 'bootstrap', 'javaScript'],
+      photo: 'assets/projects_screenshot/tic_tac_toe.png',
+      skills: ['html', 'css', 'bootstrap', 'JavaScript'],
       link: 'https://mohammed-mujib.github.io/tic_tac_toe_game/',
     },
     {
       name: 'Currency-Converter ',
-      photo: '',
-      skills: ['html', 'css', 'bootstrap', 'javaScript'],
+      photo: 'assets/projects_screenshot/Currency-Converte.png',
+      skills: ['html', 'css', 'bootstrap', 'JavaScript'],
       link: 'https://mohammed-mujib.github.io/Currency-Converter/',
     },
   ];
